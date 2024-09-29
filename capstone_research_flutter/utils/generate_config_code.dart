4ffc90f9d211f24df2c3466e3e8e74d79b046099
Park, Jonghyeon <belline0124@gmail.com>
2024-09-29 11:15:12 +0900

    fix: make main application can recongize code

--- capstone_research_flutter/utils/generate_config_code.dart
+++ capstone_research_flutter/utils/generate_config_code.dart
@@ -1,23 +1,70 @@
 import 'dart:io';
+import 'dart:convert';
 import 'package:qr/qr.dart';
 import 'package:capstone_research_flutter/classes/initializing.dart' as init;
 import 'package:capstone_research_flutter/classes/serializables/config.dart';
 import 'package:capstone_research_flutter/classes/serializables/glass_connection_config.dart';
 import 'package:capstone_research_flutter/classes/serializables/processor_connection_config.dart';
 
+const int configMargin = 4;
+const int configMarginX = configMargin;
+const int configMarginY = configMargin ~/ 2;
+
 void main() {
-  String serialized = Config(
+  String serialized = jsonEncode(Config(
     connectionConfig: ConnectionConfig(
       glassConnectionConfig: GlassConnectionConfig(address: init.glassConnectionConfigAddress),
       processorConnectionConfig: ProcessorConnectionConfig(address: init.processorConnectionConfigAddress)
     )
-  ).toJson().toString();
-  final qrCode = QrCode(5, QrErrorCorrectLevel.L)
+  ).toJson());
+  
+  final qrCode = QrCode(6, QrErrorCorrectLevel.L)
     ..addData(serialized);
   final qrImage = QrImage(qrCode);
-  for (var x = 0; x < qrImage.moduleCount; x++) {
-    for (var y = 0; y < qrImage.moduleCount; y++) {
-      stdout.write(qrImage.isDark(y, x) ? '██' : '  ');
+
+  int textWidth = qrImage.moduleCount;
+  int textHeight = qrImage.moduleCount ~/ 2 + qrImage.moduleCount % 2;
+  print('');
+  // margin
+  for (var my = 0; my < configMarginY; my++) {
+    for (var mx = 0; mx < textWidth + configMarginX * 2; mx++) {
+      stdout.write('█');
+    }
+    print('');
+  }
+
+  for (var y = 0; y < textHeight; y++) {
+    // margin
+    for (var mx = 0; mx < configMarginX; mx++) {
+      stdout.write('█');
+    }
+
+    // content
+    for (var x = 0; x < textWidth; x++) {
+      if (y * 2 + 1 < qrImage.moduleCount) {
+        stdout.write(
+          qrImage.isDark(y * 2, x) ? (
+            qrImage.isDark(y * 2 + 1, x) ? ' ' : '▄'
+          ) : (
+            qrImage.isDark(y * 2 + 1, x) ? '▀' : '█'
+          )
+        );
+      } else {
+        stdout.write(qrImage.isDark(y * 2, x) ? '▄' : '█');
+      }
+    }
+
+    // margin
+    for (var mx = 0; mx < configMarginX; mx++) {
+      stdout.write('█');
+    }
+    print('');
+  }
+
+  // margin
+  for (var my = 0; my < configMarginY; my++) {
+    for (var mx = 0; mx < textWidth + configMarginX * 2; mx++) {
+      stdout.write('█');
     }
     print('');
   }
